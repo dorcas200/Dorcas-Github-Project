256ec3fb2175795bc14a2c5befee975d343c76f3
dorcas200 <adayilobussa28@gmail.com>
2019-10-30 07:45:26 -0700

    First Project

---
+++ cmp 101 assignment 2.0.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 08:11:11 2019
+
+@author: STEPHANIE EDOH
+"""
+
+score = 45
+if (score>=70) and (score<=100):
+    print ('A')
+elif (score>=60) and (score<=69):
+    print ('B')
+elif (score>=50) and (score<=59):
+    print ('C')
+elif (score>=45) and (score<=49):
+    print ('D')
+elif (score>=40) and (score<=44):
+    print ('E')
+elif (score>=0) and (score<=39):
+    print ('F')
